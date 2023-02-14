6d981ebaae7f517cb0ec570ed28a42fe61439b5f
Maksim <korotaevma@gmail.com>
2023-02-14 21:23:13 +0500

    Решение4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 44;
+int b = 5;
+int c = 78;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
