baa96f00a953a6bfbda4566c06f748697dc7251c
Maksim <korotaevma@gmail.com>
2023-02-14 20:51:43 +0500

    Решение

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = -9;
+int b = -3;
+if(a > b)
+{
+    Console.WriteLine("наибольшее значение" + a);
+    Console.WriteLine("наименьшее значение" + b);
+}
+else
+{
+   Console.WriteLine("наибольшее значение" + b);
+   Console.WriteLine("наименьшее значение" + a); 
+}
